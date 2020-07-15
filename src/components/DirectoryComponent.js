11c14da2b72bc947be5cbfad7fd0589a286bf36a
Jack Stewart <javastew93@gmail.com>
2020-07-14 17:08:53 -0700

    React Controlled Forms

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle,Breadcrumb, BreadcrumbItem } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import React from "react";
 
-function RenderDirectoryItem({campsite}) {
+
+function RenderDirectoryItem({ campsite }) {
   return (
-      <Card>
-          <Link to={`/directory/${campsite.id}`}>
-              <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-              <CardImgOverlay>
-                  <CardTitle>{campsite.name}</CardTitle>
-              </CardImgOverlay>
-          </Link>
-      </Card>
+    <Card>
+      <Link to={`/directory/${campsite.id}`}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
+    </Card>
   );
 }
 
@@ -26,6 +36,18 @@ function Directory(props) {
 
   return (
     <div className="container">
+      <div className="row">
+        <div className="col">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Directory</BreadcrumbItem>
+          </Breadcrumb>
+          <h2>Directory</h2>
+          <hr />
+        </div>
+      </div>
       <div className="row">{directory}</div>
     </div>
   );
